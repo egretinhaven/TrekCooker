f73edfbb446fbfda4a63b935e872642d356f4f53
egretinhaven <haiyun.lu@gmail.com>
2015-08-14 17:48:54 -0400

    modified InitReader.cpp to enable replacing env variables add CsI plugin skeleton

---
+++ src/plugins/detector/CsI/src/CsI_fitWave.cpp
@@ -0,0 +1,23 @@
+#include <Det_CsI.h>
+
+#include<cmath>
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+Long_t Det_CsI::histos_fit(){
+  cout<<"histos_fit"<<endl;
+  return 0;
+}
+Long_t Det_CsI::startup_fit(){
+  cout<<"startup_fit"<<endl;
+  return 0;
+}
+Long_t Det_CsI::process_fit(){
+  cout<<"process_fit"<<endl;
+  return 0;
+}
+Long_t Det_CsI::finalize_fit(){
+  cout<<"finalize_fit"<<endl;
+  return 0;
+}
---
+++ src/plugins/detector/CsI/src/include/Det_CsI.h
@@ -0,0 +1,52 @@
+#ifndef __DET_CSI__
+#define __DET_CSI__
+#include "TObject.h"
+#include "Plugin.h"
+#include "TTree.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TF1.h"
+#include "csitree.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <utility>
+#include <map>
+typedef std::pair<UInt_t,UInt_t> IdCsI;
+class Det_CsI:public Plugin
+{
+ private:
+
+  CRTFitCsI *treeFit;			/// Output tree for CSI data
+  CRTRawCsI *treeRaw;			/// Input tree with CSI raw data
+  //
+  // Detector parameters set in init file
+  //
+
+  // constants 
+
+  std::map<IdCsI,UInt_t> mapCsI;
+
+ public:
+  Det_CsI(TTree *in_,TTree *out_,TFile *inf_, TFile * outf_,TObject *p_);
+  virtual ~Det_CsI();
+
+  // Main CsI analysis methods
+  Long_t histos();
+  Long_t startup();
+  Long_t process();
+  Long_t done();
+
+  // CsI fit
+  Long_t histos_fit();
+  Long_t startup_fit();
+  Long_t process_fit();
+  Long_t finalize_fit();
+  Long_t setIdCsI(std::map<IdCsI,UInt_t>& mapCsI);
+  virtual Long_t cmdline(char * cmd);
+
+  ClassDef(Det_CsI,1);
+    };
+
+#endif
